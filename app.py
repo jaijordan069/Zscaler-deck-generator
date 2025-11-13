144e9d34a91c8ddc8e14ebd66239684dcea0aba3
jaijordan069 <151782048+jaijordan069@users.noreply.github.com>
2025-11-14 00:13:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from pptx import Presentation
 from pptx.util import Inches, Pt
 from pptx.dml.color import RGBColor
 from pptx.enum.text import PP_ALIGN
-from pptx.enum.table import MSO_TABLE_STYLE
 from datetime import datetime
 import io
 
